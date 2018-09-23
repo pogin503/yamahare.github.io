c4c48d883b41132f5b57faf68f46665fd633b65a
yamahare <jidai.norio@gmail.com>
2018-09-23 17:06:52 +0900

    [add]autoprefixerとpug追加

--- tasks/styles.js
+++ tasks/styles.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
 const { paths } = require('./config')
 
 function styles() {
   return gulp.src(paths.styles.src)
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer(['last 2 versions', 'ie >= 8', 'Android >= 4', 'iOS >= 8']))
     .pipe(gulp.dest(paths.styles.dest))
 }
 
