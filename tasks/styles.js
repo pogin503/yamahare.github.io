28c1b6f7cd25ec50abd19fff949b1acdfeeddc46
yamahare <jidai.norio@gmail.com>
2018-09-24 15:22:24 +0900

    [update]normalizeとsassGlobを追加

--- tasks/styles.js
+++ tasks/styles.js
@@ -3,15 +3,19 @@ const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
 const cssnano = require('cssnano')
 const autoprefixer = require('autoprefixer')
+const normalize = require('postcss-normalize')
+const sassGlob = require('gulp-sass-glob')
 const { paths } = require('./config')
 // postcssのプラグイン
 
 function styles() {
   const plugins = [
+    normalize({ browsers: ["last 2 versions", "ie >= 11", "Android >= 4"] }),
     autoprefixer({ browsers: ["last 2 versions", "ie >= 11", "Android >= 4"] }),
     cssnano({ autoprefixer: false })
   ];
   return gulp.src(paths.styles.src)
+    .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(plugins))
     .pipe(gulp.dest(paths.styles.dest))
