271a533e68a643d901ddd5f25a7ab4d916816fcf
yamahare <jidai.norio@gmail.com>
2018-12-30 00:23:02 +0900

    [clean]gulp-filterとgulp-rev-delete-originalのパッケージを追加。

--- tasks/rev.js
+++ tasks/rev.js
@@ -1,11 +1,17 @@
 const gulp = require('gulp')
 const rev = require('gulp-rev');
+const revdel = require('gulp-rev-delete-original');
+const filter = require('gulp-filter');
 const { paths } = require('./config')
-// postcssのプラグイン
 
 function add_hash () {
+  const excepHtml = filter(['**/*', '!**/index.html'], {restore: true});
+
   return gulp.src(paths.manifest.src)
-    .pipe(rev()) // Rename all files except index.html
+    .pipe(excepHtml)
+    .pipe(rev())         // Rename all files except index.html
+    .pipe(revdel())
+    .pipe(excepHtml.restore)
     .pipe(gulp.dest(paths.manifest.dest))
     .pipe(rev.manifest())
     .pipe(gulp.dest(paths.manifest.dest));
